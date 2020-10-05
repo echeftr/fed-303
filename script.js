9f93c5eef52238fa743025b56f0197aeb7bebfa4
echeftr <69512919+echeftr@users.noreply.github.com>
2020-10-05 11:06:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+document.write("Welcome to our club!");
+
+const minAge = 18;
+if (minAge <= 18) {
+  document.write("Hello welkom. U mag naar binnen.");
+} else {
+  document.write("Helaas ben je nog te jong om naar binnen te komen.");
+}
+
+const isFemale = false;
+if (isFemale) {
+  document.write("another Lady in tha house.");
+} else {
+  document.write("Not a lady.");
+}
+
+const driverStatus = 0;
+if (driverStatus == 0) {
+  document.write("Bob and can drive.");
+} else {
+  document.write("Alcohol consumption, can not drive!");
+}
